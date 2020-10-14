a54e1309e446645b15f1615ec393233b80b14663
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-14 14:50:50 -0700

    initial header layout

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "../Input";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Assignment 2</h1>
+      <Input />
+    </header>
+  );
+};
+
+export default Header;
