b9ddb4520d72b3d6ca2e3250f4d6a834d294fd4b
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-14 15:22:25 -0700

    created gradient background for header and started on main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.scss";
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Main />
       </div>
     );
   }
