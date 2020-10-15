7faf2324c50576af35d8851c5d44a866bd7d2f60
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-14 17:28:08 -0700

    created animation background layout

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,6 +4,7 @@ import Input from "../Input";
 const Header = () => {
   return (
     <header>
+      <div className="animationArea"></div>
       <h1>Assignment 2</h1>
       <Input />
     </header>
