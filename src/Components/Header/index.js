29de841ca5bff4bd99aae6ccb9a224e99598ef5d
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-14 19:02:24 -0700

    animation for header complete

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,7 +4,16 @@ import Input from "../Input";
 const Header = () => {
   return (
     <header>
-      <div className="animationArea"></div>
+      <div className="animationArea">
+        <ul class="blocks">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
       <div className="contentWrapper">
         <h1>Assignment 2</h1>
         <Input />
