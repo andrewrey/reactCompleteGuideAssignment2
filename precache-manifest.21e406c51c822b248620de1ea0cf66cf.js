5b97ae28be755d20e9abd675ac1f1f3b5890843b
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-16 18:06:04 -0700

    Updates

---
+++ precache-manifest.21e406c51c822b248620de1ea0cf66cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b80657775281c17192e353f4c52833",
+    "url": "/reactCompleteGuideAssignment2/index.html"
+  },
+  {
+    "revision": "4ed098513f572342f579",
+    "url": "/reactCompleteGuideAssignment2/static/css/main.7620dbec.chunk.css"
+  },
+  {
+    "revision": "d9fc119757ac83d714cc",
+    "url": "/reactCompleteGuideAssignment2/static/js/2.f14ddfa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactCompleteGuideAssignment2/static/js/2.f14ddfa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed098513f572342f579",
+    "url": "/reactCompleteGuideAssignment2/static/js/main.e6839b0e.chunk.js"
+  },
+  {
+    "revision": "b711827a8f98306e624a",
+    "url": "/reactCompleteGuideAssignment2/static/js/runtime-main.c169389c.js"
+  },
+  {
+    "revision": "cd33634b6ef7de80cdd24d7330bb9fa5",
+    "url": "/reactCompleteGuideAssignment2/static/media/pexels-markus-spiske-3872166.cd33634b.jpg"
+  }
+]);
